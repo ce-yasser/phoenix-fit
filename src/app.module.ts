6cfd627bc8d746803e0d82a9fc3ba3d34194c8f8
Ahmed Yasser <ahmed.yasser@almatar.com>
2026-07-18 12:32:43 +0300

    Restructure folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from '@modules/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from '@modules/auth/auth.module';
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule],
