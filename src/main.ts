8f4da5e0a0865cd441032f9780e2acc2cd26a227
Ahmed Yasser <ahmed.yasser@almatar.com>
2026-07-29 02:48:42 +0300

    enable cors for port 3000

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,11 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   await app.listen(Number(configService.get<string>('PORT') ?? 5500));
 }
